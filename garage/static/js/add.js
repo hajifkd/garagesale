fe31ac4b8edcaf164428552508372c8b070b014e
Hajime Fukuda <haji.fkd@gmail.com>
2022-03-19 18:56:20 -0700

    another cam

--- garage/static/js/add.js
+++ garage/static/js/add.js
@@ -9,7 +9,7 @@ window.onload = () => {
         video: {
             width: 300,
             height: 200,
-            //facingMode: { exact: "environment" }  // リアカメラを利用する場合
+            facingMode: { exact: "environment" }  // リアカメラを利用する場合
         }
     };
 
